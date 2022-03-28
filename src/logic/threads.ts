1e58b17e9fb5dafba276ef7447e1981eb70f4f78
Jamie Bray <jamie@jamiebray.xyz>
2022-03-29 03:01:04 +0800

    7.9s Just changing thread and job counts around

--- src/logic/threads.ts
+++ src/logic/threads.ts
@@ -1,2 +1,2 @@
-export const PROBABLY_OPTIMAL_THREAD_COUNT = 16;
-export const PROBABLY_OPTIMAL_JOB_COUNT = 16;
+export const PROBABLY_OPTIMAL_THREAD_COUNT = navigator.hardwareConcurrency * 4;
+export const PROBABLY_OPTIMAL_JOB_COUNT = PROBABLY_OPTIMAL_THREAD_COUNT * 2;
