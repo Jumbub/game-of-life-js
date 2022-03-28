0b5af42254a6941ac493af3fd4c5d31acc381833
Jamie Bray <jamie@jamiebray.xyz>
2022-03-28 23:17:55 +0800

    working

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,31 @@
  * @type {import('vite').UserConfig}
  */
 const config = {
+  mode: 'production',
   server: {
-    headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Opener-Policy': 'same-origin',
-    },
+    port: 8080,
   },
+  build: {
+    target: 'esnext',
+    polyfillModulePreload: false,
+    sourcemap: false,
+    minify: false, // TODO: try minified
+  },
+  worker: {
+    format: 'es',
+  },
+  plugins: [
+    {
+      name: 'configure-response-headers',
+      configureServer: server => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+          next();
+        });
+      },
+    },
+  ],
 };
 
 export default config;
