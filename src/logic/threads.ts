f8ffb465515cb225bb300a14dbbc4feaf37fdb49
Jamie Bray <jamie@jamiebray.xyz>
2022-03-31 01:23:33 +0800

    Reducing thread count to increase reliability of the renderer

--- src/logic/threads.ts
+++ src/logic/threads.ts
@@ -1,2 +1,2 @@
-export const PROBABLY_OPTIMAL_THREAD_COUNT = navigator.hardwareConcurrency * 4;
-export const PROBABLY_OPTIMAL_JOB_COUNT = PROBABLY_OPTIMAL_THREAD_COUNT * 2;
+export const PROBABLY_OPTIMAL_THREAD_COUNT = navigator.hardwareConcurrency * 2;
+export const PROBABLY_OPTIMAL_JOB_COUNT = PROBABLY_OPTIMAL_THREAD_COUNT * 4;
